607530e70ae2e49c418bda0aac7f63f07ad8f24f
IvanBerrettini <ivanberrettini@gmail.com>
2022-04-21 10:16:04 -0300

    arreglado poisson

--- TP1-Generador de numeros pseudoaleatoreos/Controllers/Poisson.cs
+++ TP1-Generador de numeros pseudoaleatoreos/Controllers/Poisson.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace TP1_Generador_de_numeros_pseudoaleatoreos.Controllers
 {
@@ -29,15 +30,37 @@ namespace TP1_Generador_de_numeros_pseudoaleatoreos.Controllers
             List<double> probabilidades = new List<double>();
             for (int i = 0; i < listaConValores.Count; i++)
             {
-                double probabilidad = Math.Truncate((Math.Pow(lambda,listaConValores[i])* Math.Exp(-lambda))/ factorial(Convert.ToInt32(listaConValores[i])) * 10000) / 10000;
+                double numerador1 = Math.Pow(lambda, listaConValores[i]);
+                double numerador2 = Math.Exp(-lambda);
+                //double denominador = factorial((long)(listaConValores[i]));
+                double factorial = 1;
+                for (double j = listaConValores[i]; j >= 1; j--)
+                {
+                    factorial *= j;
+                }
+                double probabilidad = (numerador1 * numerador2) / factorial;
+                //double probabilidad = Math.Truncate((Math.Pow(lambda,listaConValores[i])* Math.Exp(-lambda))/ factorial(Convert.ToInt32(listaConValores[i])) * 100000000) / 100000000;
                 probabilidades.Add(probabilidad);
+                
             }
             return probabilidades;
         }
 
-        public int factorial(int n)
+        public long factorial(long n)
         {
             return (n == 1 || n == 0) ? 1 : n * factorial(n - 1);
+                
+            //long n = 0;
+            //while (num > 0)
+            //{
+            //     n = num;
+            //    for (long i = n - 1; i > 0; i--)
+            //    {
+            //        n *= i;
+            //    }
+            //    num--;
+            //}
+            //return n;
         }
 
 
@@ -56,7 +79,7 @@ namespace TP1_Generador_de_numeros_pseudoaleatoreos.Controllers
 
                 do
                 {
-                    double U = Math.Truncate(rnd.NextDouble() * 10000) / 10000;
+                    double U = rnd.NextDouble();// Math.Truncate( * 100000000) / 100000000;
                     P *= U;
                     X += 1;
                 } while (P >= A);
